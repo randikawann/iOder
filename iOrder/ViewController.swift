53e62367364de9c9aca56ca3861a6b53027710a4
Randika Wanninayaka <randikawann@gmail.com>
2019-09-21 07:00:50 +0530

    Initial Commit

---
+++ iOrder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOrder
+//
+//  Created by Randika Wanninayaka on 9/21/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
